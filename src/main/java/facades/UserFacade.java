314fa2ff096fb581388d312007b55499ff001896
cherr <cherr@10.50.137.172>
2016-11-01 12:02:40 +0100

    change to create and do insert data from main

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -17,46 +17,12 @@ public class UserFacade implements IUserFacade {
 
   public UserFacade(EntityManagerFactory emf) {
     this.emf = emf;   
-    insertData();
   }
 
   private EntityManager getEntityManager() {
     return emf.createEntityManager();
   }
   
-  
-  private void insertData(){
-      EntityManager em = getEntityManager();
-      
-      try {
-      System.out.println("Creating TEST Users");
-      if (em.find(User.class, "user") == null) {
-        em.getTransaction().begin();
-        Role userRole = new Role("User");
-        Role adminRole = new Role("Admin");
-        User user = new User("user", "test");
-        user.addRole(userRole);
-        User admin = new User("admin", "test");
-        admin.addRole(adminRole);
-        User both = new User("user_admin", "test");
-        both.addRole(userRole);
-        both.addRole(adminRole);
-        em.persist(userRole);
-        em.persist(adminRole);
-        em.persist(user);
-        em.persist(admin);
-        em.persist(both);
-        em.getTransaction().commit();
-        System.out.println("Created TEST Users");
-      }
-    } catch (Exception ex) {
-      Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-      em.getTransaction().rollback();
-    } finally {
-      em.close();
-    }
-  }
-
   @Override
   public IUser getUserByUserId(String id) {
     EntityManager em = getEntityManager();
