0c963fd8a22ba8091f35f4e2bb4b8892b0a109f3
rasmus <rasmus@10.50.130.202>
2016-11-02 12:30:21 +0100

    diagram added, shop.sql extended to create 2 shops

--- src/main/resources/scripts/shops.sql
+++ src/main/resources/scripts/shops.sql
@@ -1,1 +1,7 @@
+INSERT INTO `CA3DB`.`ADDRESS` (`AdditionalInfo`, `Street`, `CityInfo_id`) VALUES ('5', 'Ravsnborggade', '2200');
+INSERT INTO `CA3DB`.`SHOPMAPPOSITION` (`X`, `Y`) VALUES ('25', '25');
 INSERT INTO `CA3DB`.`SHOP` (`CATEGORY`, `DESCRIPTION`, `EMAIL`, `GOOGLESHOPID`, `NAME`, `PHONE`, `WEBSITE`, `ADRESS_id`, `POS_ID`) VALUES ('WINE', 'very nice wineshop', 'shop@vintrovin.dk', 'ChIJiwsILgdTUkYRPRASlR0END0', 'VINTRO', '51220002', 'www.vintrovin.dk', '30', '1');
+
+INSERT INTO `CA3DB`.`ADDRESS` (`AdditionalInfo`, `Street`, `CityInfo_id`) VALUES ('48', 'Nørrebrograde', '2200');
+INSERT INTO `CA3DB`.`SHOPMAPPOSITION` (`X`, `Y`) VALUES ('25', '25');
+INSERT INTO `CA3DB`.`SHOP` (`CATEGORY`, `DESCRIPTION`, `GOOGLESHOPID`, `NAME`, `PHONE`, `WEBSITE`, `ADRESS_id`, `POS_ID`) VALUES ('BEER', '2 floors pool, fussball etc.', 'ChIJe-fNI6pTUkYRDQ2kmIVrgKE', 'Temple bar', '35374414', 'https://www.facebook.com/templebarCPH/', '31', '2');
