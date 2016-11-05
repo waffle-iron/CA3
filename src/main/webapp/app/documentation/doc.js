2d28bb58eb27ec3c4b7b82e0f022ed55a72b7cba
cherr <cherr@10.50.137.172>
2016-11-05 22:31:42 +0100

    Finalized Documentation

--- src/main/webapp/app/documentation/doc.js
+++ src/main/webapp/app/documentation/doc.js
@@ -3,8 +3,23 @@
 angular.module('myApp.documentation', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/doc', {
+  $routeProvider
+  .when('/doc', {
     templateUrl: 'app/documentation/documentation.html'
-  });
+  })
+            .when("/desc", {
+                templateUrl: "app/documentation/descDoc.html"
+            })
+            .when("/test", {
+                templateUrl: "app/documentation/testDoc.html"
+            })
+            .when("/sys", {
+                templateUrl: "app/documentation/sysDoc.html"
+            })
+            .when("/task", {
+                templateUrl: "app/documentation/taskDoc.html"
+            })
+            .otherwise({
+                templateUrl: '/doc'});
 }]);
 
