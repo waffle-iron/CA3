e558ce8a4be960af3bfdefbc9182e384d1906469
rasmus <rasmus@10.50.130.191>
2016-11-16 10:53:16 +0100

    git ignore local change

--- src/test/java/test/FacadeTest.java
+++ src/test/java/test/FacadeTest.java
@@ -57,7 +57,7 @@ public class FacadeTest {
     }
   
   @Test  
-  public void dummyTest(){
+  public void dummyTest(){ //sdfsdf
     assertTrue(true);  //Really "nothing" to test in the initial version of the seed
   }
   
